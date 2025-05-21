01192521e8ce6ba4ef07dd7506f7d4c63d200cf8
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2025-05-21 23:25:53 +0100

    refactor(smoke-test): remove useless anyhow

--- crates/smoke-test/src/main.rs
+++ crates/smoke-test/src/main.rs
@@ -2,7 +2,7 @@ use smoke_test::server::run;
 use smoke_test::telemetry::init_subscriber;
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() -> std::io::Result<()> {
     init_subscriber();
     run().await?;
     Ok(())
