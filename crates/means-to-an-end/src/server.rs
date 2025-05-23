6f7324d6450bbf52a5aa7d76e6bc48faaac371bd
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2025-05-23 20:32:18 +0100

    feat: solve means to an end

--- crates/means-to-an-end/src/server.rs
+++ crates/means-to-an-end/src/server.rs
@@ -1,7 +1,14 @@
-use std::str;
+use std::{collections::BTreeMap, net::SocketAddr, str};
+
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 use tracing::{event, instrument, Level};
+
+#[derive(Debug)]
+enum Message {
+    Insert,
+    Query,
+}
 
 #[instrument]
 pub async fn run() -> std::io::Result<()> {
@@ -17,9 +24,9 @@ async fn serve(address: &str) -> std::io::Result<()> {
     event!(Level::INFO, "listening on {address}");
     let listener = TcpListener::bind(address).await?;
     loop {
-        let (socket, _) = listener.accept().await?;
+        let (socket, src) = listener.accept().await?;
         tokio::spawn(async move {
-            process(socket)
+            process(socket, &src)
                 .await
                 .unwrap_or_else(|error| event!(Level::ERROR, "{error:?}"));
         });
@@ -27,15 +34,51 @@ async fn serve(address: &str) -> std::io::Result<()> {
 }
 
 #[instrument]
-async fn process(mut stream: TcpStream) -> std::io::Result<()> {
+async fn process(mut stream: TcpStream, src: &SocketAddr) -> std::io::Result<()> {
     event!(Level::DEBUG, "handling data from {}", stream.peer_addr()?);
-    let mut buffer = [0; 1024];
+    let mut storage = BTreeMap::new();
     loop {
-        let nbytes = stream.read(&mut buffer).await?;
-        if nbytes == 0 {
-            event!(Level::DEBUG, "connection closed");
-            return Ok(());
+        let mut header = [0; 1];
+        stream.read_exact(&mut header).await?;
+        let msg = match header[0] as char {
+            'I' => Message::Insert,
+            'Q' => Message::Query,
+            sym => {
+                event!(Level::ERROR, "Unknow message header {sym}");
+                break;
+            }
+        };
+        let mut op_1 = [0; 4];
+        stream.read_exact(&mut op_1).await?;
+        let op_1 = i32::from_be_bytes(op_1);
+        let mut op_2 = [0; 4];
+        stream.read_exact(&mut op_2).await?;
+        let op_2 = i32::from_be_bytes(op_2);
+        match msg {
+            Message::Insert => {
+                event!(Level::DEBUG, "insert {op_1}: {op_2}");
+                storage.insert(op_1, op_2);
+            }
+            Message::Query => {
+                if op_1 > op_2 {
+                    stream.write_all(&0i32.to_be_bytes()).await?;
+                    stream.flush().await?;
+                    continue;
+                }
+                let (mut count, mut sum) = (0, 0);
+                for (_, item) in storage.range(op_1..=op_2) {
+                    count += 1;
+                    sum += *item as i128;
+                }
+                let mean = if count == 0 { 0 } else { sum / count };
+                event!(Level::DEBUG, "query {mean}");
+                stream.write(&(mean as i32).to_be_bytes()).await?;
+                stream.flush().await?;
+            }
         }
-        stream.write_all(&buffer[..nbytes]).await?;
     }
+
+    event!(Level::INFO, "shutting down stream {src}");
+    stream.shutdown().await?;
+    Ok(())
 }
